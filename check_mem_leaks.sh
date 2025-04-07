f4813fc4920ded13eb4ea6ea328fb9dc69f4da88
kiyoshika <zachlweaver00@gmail.com>
2025-04-07 04:58:38 +0000

    update leak check file

--- check_mem_leaks.sh
+++ check_mem_leaks.sh
@@ -1,1 +1,12 @@
-find build/test -type f -perm /u=x,g=x,o=x -exec valgrind --leak-check=full --error-exitcode=-1 {} \;
+#!/bin/bash
+set -e
+
+EXIT_CODE=0
+
+for test_bin in $(find build/test -type f -perm /111); do
+  echo "Running valgrind on $test_bin"
+  valgrind --leak-check=full --error-exitcode=1 "$test_bin" || EXIT_CODE=1
+done
+
+exit $EXIT_CODE
+
