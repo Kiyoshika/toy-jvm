2357261b4eb2245e68ea4a3bfed76d599927e20d
kiyoshika <zachlweaver00@gmail.com>
2025-04-07 03:34:11 +0000

    update README.md

--- style_code.sh
+++ style_code.sh
@@ -1,2 +1,4 @@
+echo "Running code formatter..."
 find src/ -iname '*.c' | xargs clang-format --style=Mozilla -i
 find include/ -iname '*.h' | xargs clang-format --style=Mozilla -i
+echo "Finished formatting."
