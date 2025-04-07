ed36844eb57c4ef8bc0d9ba6b0cce5c8b3af3772
kiyoshika <zachlweaver00@gmail.com>
2025-04-07 05:30:26 +0000

    fixup local tests and mem leak script

--- check_mem_leaks.sh
+++ check_mem_leaks.sh
@@ -5,7 +5,7 @@ EXIT_CODE=0
 
 for test_bin in $(find build/test -type f -perm /111); do
   echo "Running valgrind on $test_bin"
-  valgrind --leak-check=full --error-exitcode=1 "$test_bin" || EXIT_CODE=1
+  valgrind --leak-check=full --track-origins=yes --error-exitcode=1 "$test_bin" || EXIT_CODE=1
 done
 
 exit $EXIT_CODE
